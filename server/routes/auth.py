79431a8d5065e9f173287c563d83eb2d5b6e6ea7
Riyan-Mo <Riyan8308151899@gmail.com>
2025-11-16 20:57:56 +0530

    add: route for getting user details

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -5,9 +5,10 @@ import http
 from models.user import User
 from pydantic_schemas.user_create import UserCreate
 from pydantic_schemas.user_login import UserLogin
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Header
 from database import get_db
 from sqlalchemy.orm import Session
+from middleware.auth_middleware import auth_middleware
 import jwt
 
 router = APIRouter()
@@ -34,4 +35,10 @@ def login_user(user: UserLogin, db: Session=Depends(get_db)):
         raise HTTPException(400, "Incorrect password")
     token = jwt.encode({'id': user_db.id}, 'password_key')
     return {'token': token, 'user': user_db}
-    
+
+@router.get("/")
+def current_user_data(db: Session=Depends(get_db), user_dict=Depends(auth_middleware)):
+    user = db.query(User).filter(User.id==user_dict['uid']).first()
+    if not user:
+        raise HTTPException(404, 'User not found!')
+    return user
